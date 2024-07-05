25608fffe98c38e99f70f2643c150efed55c9ab9
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-05 15:37:17 -0700

    added additional token to allowlist

--- src/allowlist.py
+++ src/allowlist.py
@@ -1,9 +1,11 @@
-# zero is top priority then it moves up
+# zero is top priority then increment
 priority = {
     # tNEWM
     "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff": 0,
     # tADA
-    "": 1
+    "": 1,
+    # tDrip
+    "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d": 2,
 }
 
 policy_ids = [
@@ -11,6 +13,8 @@ policy_ids = [
     "",
     # tNEWM
     "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff",
+    # tDRIP
+    "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d",
 ]
 
 asset_names = [
@@ -18,4 +22,6 @@ asset_names = [
     "",
     # tNEWM
     "744e45574d",
+    # tDRIP
+    "7444524950",
 ]
