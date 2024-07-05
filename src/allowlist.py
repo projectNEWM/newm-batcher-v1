eb5d9e9517db6bdfc23da6031851aeff92f6238e
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-05 14:43:09 -0700

    queue datum is validated at sort time

---
+++ src/allowlist.py
@@ -0,0 +1,9 @@
+policy_ids = [
+    # tNEWM
+    "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff",
+]
+
+asset_names = [
+    # tNEWM
+    "744e45574d",
+]
---
+++ src/datums.py
@@ -0,0 +1,57 @@
+from src.allowlist import asset_names, policy_ids
+
+
+def queue_validity(datum: dict) -> bool:
+    """
+    Validate that teh queue datum is in the correct form for an order to be fulfilled.
+
+    Args:
+        datum (dict): The queue datum
+
+    Returns:
+        bool: True if valid else False
+    """
+    try:
+        # there are four fields in the queue datum
+        if len(datum['fields']) != 4:
+            return False
+
+        # wallet check
+        if len(datum['fields'][0]['fields']) != 2:
+            return False
+        if len(datum['fields'][0]['fields'][0]['bytes']) != 56:
+            return False
+        if len(datum['fields'][0]['fields'][1]['bytes']) not in [0, 56]:
+            return False
+
+        # bundle size check
+        if datum['fields'][1]['int'] <= 0:
+            return False
+
+        # incentive check
+        if len(datum['fields'][2]['fields']) != 3:
+            return False
+        if datum['fields'][2]['fields'][0]['bytes'] not in policy_ids:
+            return False
+        if datum['fields'][2]['fields'][1]['bytes'] not in asset_names:
+            return False
+        if datum['fields'][2]['fields'][2]['int'] <= 0:
+            return False
+
+        # pointer token check
+        if len(datum['fields'][3]['bytes']) != 64:
+            return False
+
+        # every thing seems good
+        return True
+
+    except KeyError:
+        # some field doesnt exist
+        return False
+
+
+def get_incentive_amount(datum: dict) -> int:
+    try:
+        return datum['fields'][2]['fields'][2]['int']
+    except KeyError:
+        return None
