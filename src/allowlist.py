de240a812094b25678a164969f7d812cb9f6e516
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-05 15:26:31 -0700

    sorting by incentive includes priority ranking

--- src/allowlist.py
+++ src/allowlist.py
@@ -1,9 +1,21 @@
+# zero is top priority then it moves up
+priority = {
+    # tNEWM
+    "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff": 0,
+    # tADA
+    "": 1
+}
+
 policy_ids = [
+    # tADA
+    "",
     # tNEWM
     "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff",
 ]
 
 asset_names = [
+    # tADA
+    "",
     # tNEWM
     "744e45574d",
 ]
