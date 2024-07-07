bbd1c90f67371843e5d38b9a0f24ce9984effa5a
logicalmechanism <logical.mechanism@protonmail.com>
2024-07-07 11:02:37 -0700

    profit endpoint requires a threshold from allowlist

--- src/allowlist.py
+++ src/allowlist.py
@@ -1,27 +1,32 @@
+thresholds = {
+    # tNEWM
+    "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff": 100000000,
+}
+
 # zero is top priority then increment
 priority = {
     # tNEWM
     "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff": 0,
-    # tADA
-    "": 1,
-    # tDrip
-    "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d": 2,
+    # # tADA
+    # "": 1,
+    # # tDrip
+    # "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d": 2,
 }
 
 policy_ids = [
-    # tADA
-    "",
     # tNEWM
     "769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff",
+    # tADA
+    # "",
     # tDRIP
-    "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d",
+    # "698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d",
 ]
 
 asset_names = [
-    # tADA
-    "",
     # tNEWM
     "744e45574d",
+    # tADA
+    # "",
     # tDRIP
-    "7444524950",
+    # "7444524950",
 ]
--- src/endpoint.py
+++ src/endpoint.py
@@ -4,7 +4,7 @@ import subprocess
 from src.address import pkh_from_address
 from src.cli import txid
 from src.datums import (bundle_to_value, cost_to_value, get_number_of_bundles,
-                        incentive_to_value)
+                        incentive_to_value, to_address)
 from src.json_file import write
 from src.redeemer import empty
 from src.utility import parent_directory_path
@@ -171,6 +171,88 @@ class Endpoint:
             tuple[dict, dict, dict, bool]: The sale, queue, batcher info, and a success flag.
         """
         refund_success_flag = False
+
+        # reference UTxOs for the scripts
+        data_ref_utxo = config['data_ref_utxo']
+        queue_ref_utxo = config['queue_ref_utxo']
+
+        # batcher pkh for signing
+        batcher_pkh = pkh_from_address(config['batcher_address'])
+
+        fee = 350000
+        fee_value = Value({"lovelace": fee})
+        execution_units = '(400000000, 1500000)'
+
+        # The parent directory for relative pathing
+        parent_dir = parent_directory_path()
+
+        # The protocol file needs to be inside of the tmp directory
+        protocol_file_path = os.path.join(parent_dir, "tmp/protocol.json")
+        out_file_path = os.path.join(parent_dir, "tmp/tx.draft")
+
+        # queue refund redeemer
+        write(empty(1), "tmp/refund-redeemer.json")
+        queue_redeemer_file_path = os.path.join(parent_dir, "tmp/refund-redeemer.json")
+
+        # datums for refund
+        queue_datum = queue_info['datum']
+
+        # queue entry owner address
+        owner_address = to_address(queue_datum, config["network"])
+
+        # queue incentive
+        incentive_value = incentive_to_value(queue_datum)
+
+        batcher_value = batcher_info['value']
+        queue_value = queue_info['value']
+
+        # if incentive not in queue then fail
+        if not queue_value.contains(incentive_value):
+            return sale_info, queue_info, batcher_info, refund_success_flag
+
+        # calculate the outbound values for sale, queue, and batcher
+        queue_out_value = queue_value - incentive_value - fee_value
+        batcher_out_value = batcher_value + incentive_value
+
+        func = [
+            'cardano-cli', 'transaction', 'build-raw',
+            '--babbage-era',
+            '--protocol-params-file', protocol_file_path,
+            '--out-file', out_file_path,
+            "--tx-in-collateral", batcher_info['txid'],
+            '--read-only-tx-in-reference', data_ref_utxo,
+            '--read-only-tx-in-reference', sale_info['txid'],
+            "--tx-in", batcher_info['txid'],
+            '--tx-in', queue_info['txid'],
+            '--spending-tx-in-reference', queue_ref_utxo,
+            '--spending-plutus-script-v2',
+            '--spending-reference-tx-in-inline-datum-present',
+            '--spending-reference-tx-in-execution-units', execution_units,
+            '--spending-reference-tx-in-redeemer-file', queue_redeemer_file_path,
+            "--tx-out", batcher_out_value.to_output(config['batcher_address']),
+            '--tx-out', queue_out_value.to_output(owner_address),
+            '--required-signer-hash', batcher_pkh,
+            '--fee', str(fee)
+        ]
+
+        # this saves to out file
+        p = subprocess.Popen(func, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, errors = p.communicate()
+
+        print('output', output)
+        print('errors', errors)
+
+        # do someting
+        refund_success_flag = True
+
+        intermediate_txid = txid(out_file_path)
+
+        queue_info['txid'] = intermediate_txid + "#1"
+        queue_info['value'] = queue_out_value
+
+        batcher_info['txid'] = intermediate_txid + "#0"
+        batcher_info['value'] = batcher_out_value
+
         return sale_info, queue_info, batcher_info, refund_success_flag
 
     @staticmethod
@@ -186,4 +268,64 @@ class Endpoint:
             tuple[dict, bool]: The UTxO with the batcher token and a success flag
         """
         profit_success_flag = False
-        return {}, profit_success_flag
+
+        # batcher pkh for signing
+        batcher_pkh = pkh_from_address(config['batcher_address'])
+
+        # The parent directory for relative pathing
+        parent_dir = parent_directory_path()
+
+        # The protocol file needs to be inside of the tmp directory
+        protocol_file_path = os.path.join(parent_dir, "tmp/protocol.json")
+        out_file_path = os.path.join(parent_dir, "tmp/tx.draft")
+
+        fee = 350000
+        fee_value = Value({"lovelace": fee})
+
+        batcher_out_value = Value({"lovelace": 5000000, config["batcher_policy"]: {config["batcher_asset"]: 1}})
+        total_batcher_value = Value({})
+        tx_in_list = []
+        returning_batcher_info = None
+        for batcher_info in batcher_infos:
+            # assume only one utxo has the batcher policy
+            if batcher_info['value'].exists(config["batcher_policy"]):
+                returning_batcher_info = batcher_info
+            else:
+                # only do the profit if some other utxo contains at least 5 ada
+                if not batcher_info['value'].contains(Value({"lovelace": 5000000})):
+                    return returning_batcher_info, profit_success_flag
+            total_batcher_value += batcher_info['value']
+            tx_in_list.append("--tx-in")
+            tx_in_list.append(batcher_info['txid'])
+        batcher_profit_value = total_batcher_value - batcher_out_value - fee_value
+
+        func = [
+            'cardano-cli', 'transaction', 'build-raw',
+            '--babbage-era',
+            '--protocol-params-file', protocol_file_path,
+            '--out-file', out_file_path,
+        ]
+        func += tx_in_list
+        func += [
+            "--tx-out", batcher_out_value.to_output(config['batcher_address']),
+            "--tx-out", batcher_profit_value.to_output(config['profit_address']),
+            '--required-signer-hash', batcher_pkh,
+            '--fee', str(fee)
+        ]
+
+        # this saves to out file
+        p = subprocess.Popen(func, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, errors = p.communicate()
+
+        print('output', output)
+        print('errors', errors)
+
+        # check output / errors, if all good assume true here
+        profit_success_flag = True
+
+        intermediate_txid = txid(out_file_path)
+
+        returning_batcher_info['txid'] = intermediate_txid + "#0"
+        returning_batcher_info['value'] = batcher_out_value
+
+        return returning_batcher_info, profit_success_flag
