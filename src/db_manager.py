10d01043e7eb2ea0d5946bde0e9c632243f3a0c0
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-07 18:39:26 -0700

    reference scripts now have db, need to create load function

--- src/db_manager.py
+++ src/db_manager.py
@@ -2,6 +2,7 @@ from src.db.batcher_db_manager import BatcherDbManager
 from src.db.data_db_manager import DataDbManager
 from src.db.oracle_db_manager import OracleDbManager
 from src.db.queue_db_manager import QueueDbManager
+from src.db.reference_db_manager import ReferenceDbManager
 from src.db.sale_db_manager import SaleDbManager
 from src.db.seen_db_manager import SeenDbManager
 from src.db.status_db_manager import StatusDbManager
@@ -15,6 +16,7 @@ class DbManager:
         self.data = DataDbManager(db_file)
         self.oracle = OracleDbManager(db_file)
         self.queue = QueueDbManager(db_file)
+        self.reference = ReferenceDbManager(db_file)
         self.sale = SaleDbManager(db_file)
         self.seen = SeenDbManager(db_file)
         self.status = StatusDbManager(db_file)
@@ -25,12 +27,15 @@ class DbManager:
         self.data.initialize()
         self.oracle.initialize()
         self.queue.initialize()
+        self.reference.initialize()
         self.sale.initialize()
         self.seen.initialize()
         self.status.initialize()
         self.vault.initialize()
         # load the start status from config
         self.status.load(config)
+        # load the reference data here
+        self.reference.load(config)
         # create the start oracle from empty
         self.oracle.create("", {}, Value({}))
         # create the start data from empty
@@ -41,6 +46,7 @@ class DbManager:
         self.data.cleanup()
         self.oracle.cleanup()
         self.queue.cleanup()
+        self.reference.cleanup()
         self.sale.cleanup()
         self.seen.cleanup()
         self.status.cleanup()
