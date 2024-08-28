0bf858f2d7a2b51e5c59d1e21ce42820aeda70a4
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-27 18:18:07 -0700

    creating a setup script to download required binaries

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 echo -e "\033[1;34m\nDownloading Required Binaries\n\033[0m"
 
+if [ -x "bin/cardano-cli" ]; then
+    echo -e "\033[1;32mcardano-cli Exists!\033[0m"
+else
+    echo "The binary does not exist or is not executable."
+fi
