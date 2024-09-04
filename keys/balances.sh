b2c20c5b029fedec3db01c1e4098b1c7db58c46e
logicalmechanism <logical.mechanism@protonmail.com>
2024-09-04 09:51:47 -0700

    prepping repo for new marketplace contracts

--- keys/balances.sh
+++ keys/balances.sh
@@ -8,7 +8,6 @@ socket_path=$(yq '.socket_path' ../config.yaml)
 network=$(yq '.network' ../config.yaml)
 cli=$(yq '.cli_path' ../config.yaml)
 
-
 echo -e "\n \033[1;34mBatcher: ${batcher_address} \033[0m \n";
 ${cli} query utxo --socket-path ${socket_path} --address ${batcher_address} ${network}
 
